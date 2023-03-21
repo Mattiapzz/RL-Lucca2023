a73a0cdc24c85c93f5c724be39df56715693e350
Mattia Piazza <mattia.piazza@unitn.it>
2023-03-21 10:57:43 +0100

    fix seba

--- Assigment1-1.py
+++ Assigment1-1.py
@@ -1,44 +1,77 @@
+'''
+Assignment 1
+
+Authors
+-------
+ - Mattia Piazza,
+ - Sebastiano Taddei.
+
+'''
+
 import numpy as np
 
 
-GAMMA = (0.5,0.85,0.9,0.99,1-1e-5,1)
+def state_transition_matrix_P1():
+    '''
+    
+    State transition matrix for policy 1.
+    
+    '''
+    
+    P1 = np.zeros((20,20))
+    for i in range(19):
+        if i%5 == 4:
+            P1[i,0] = 1
+        else:
+            P1[i,i+1] = 1
 
+    P1[19,0] = 1
 
-P1 = np.zeros((20,20))
-for i in range(0,19):
-    if i < 20:
-        P1[i,i+1] = 1
-    if i == 20:
-        P1[i,1] = 1
+    return P1
 
-P2 = np.zeros((20,20))
-for i in range(0,19):
-    if i < 20:
-        P2[i,i+1] = 1
-    if i == 5 or i == 10 or i == 15:
-        P2[i,1] = 1
+def state_transition_matrix_P2():
+    '''
+    
+    State transition matrix for policy 2.
+    
+    '''
+    
+    P2 = np.eye(20, k=1)
+    P2[19,0] = 1
 
-Vs = np.zeros((20,1))
-
-
-
-
-
-
+    return P2
 
 def pi1(s):
+    '''
+
+    Policy 1.
+
+    '''
+
     if s == 5 or s == 10 or s == 15 or s == 20:
         return 3
     else:
         return 2
     
 def pi2(s):
+    '''
+
+    Policy 2.
+
+    '''
+
     if s == 20:
         return 3
     else:
         return 2
     
-def reward(s,a):
+def reward(s, a):
+    '''
+
+    Reward function.
+
+    '''
+
     if a == 3:
         if s == 5:
             return 5
@@ -48,12 +81,16 @@ def reward(s,a):
             return 45
         elif s == 20:
             return 80
-        # else:
-        #     return 0
     else:
         return 0
     
-def evolve_state(s,policy):
+def evolve_state(s, policy):
+    '''
+
+    Evolve state according to policy.
+
+    '''
+
     a = policy(s)
     if a == 2:
         return s + 1
@@ -63,26 +100,43 @@ def evolve_state(s,policy):
         return s
 
     
-def V_pi(s0,gamma,policy):
-    sum = 0
-    s = s0
-    for k in range(1,1000):
-        sum = sum + gamma**k * reward(s,policy(s))
-        s = evolve_state(s,policy)
+def V_pi(state_0, gamma, policy):
+    '''
+
+    Compute the value function of a policy.
+
+    '''
+
+    sum   = 0
+    state = state_0
+    for k in range(10000):
+        sum   = sum + gamma**k * reward(state, policy(state))
+        state = evolve_state(state, policy)
+
     return sum
 
 def main():
-    for gamma in GAMMA:
-        print("gamma = ",gamma)
-        print("V_pi1 = ",V_pi(1,gamma,pi1))
-        print("V_pi2 = ",V_pi(1,gamma,pi2))
-        print("")
-        for i in range(1,20):
-            Vs[i-1] = reward(i,pi1(i))
-        print("V_pi1 (Bellman) = ", np.matmul( np.linalg.inv( np.eye(20)-gamma*P1 ) , Vs ) )
-    # print(
-    # 
-    # _pi2 = ",V_pi(1,0.5,pi2))
+    gammas = (0.5, 0.85, 0.9, 0.99, 1-1e-5) #, 1 -> omitted for singularity
+
+    for gamma in gammas:
+        print("-"*20)
+        print(f"gamma = {gamma}")
+        print("-"*20)
+        print(f"V_pi1 = {V_pi(1, gamma, pi1)}")
+        print(f"V_pi2 = {V_pi(1, gamma, pi2)}")
+        print("-"*20)
+
+        reward_P1 = [reward(i, pi1(i)) for i in range(1, 21)]
+        reward_P2 = [reward(i, pi2(i)) for i in range(1, 21)]
+
+        P1 = state_transition_matrix_P1()
+        P2 = state_transition_matrix_P2()
+
+        V_bell_P1 = np.linalg.inv( np.eye(20)-gamma*P1 ) @ reward_P1
+        V_bell_P2 = np.linalg.inv( np.eye(20)-gamma*P2 ) @ reward_P2
+        print(f"V_pi1 (Bellman) = {V_bell_P1[0]}")
+        print(f"V_pi2 (Bellman) = {V_bell_P2[0]}")
+        print("-"*20)
 
 if __name__ == "__main__":
     main()
