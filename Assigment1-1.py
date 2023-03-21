efcca886403745540072305907afb63627ac285c
Mattia Piazza <mattia.piazza@unitn.it>
2023-03-21 18:12:39 +0100

    exercise 1 b and 1 c

--- Assigment1-1.py
+++ Assigment1-1.py
@@ -9,6 +9,10 @@ Authors
 '''
 
 import numpy as np
+import matplotlib.pyplot as plt
+
+MAX_ITER = 10000 # maximum number of iterations (best results with 1000000)
+NUM_DASHES = 20
 
 
 def state_transition_matrix_P1():
@@ -106,25 +110,76 @@ def V_pi(state_0, gamma, policy):
     Compute the value function of a policy.
 
     '''
-
     sum   = 0
     state = state_0
-    for k in range(10000):
+    for k in range(MAX_ITER):
         sum   = sum + gamma**k * reward(state, policy(state))
         state = evolve_state(state, policy)
-
     return sum
 
-def main():
-    gammas = (0.5, 0.85, 0.9, 0.99, 1-1e-5) #, 1 -> omitted for singularity
+def Q_pi(state_0, action_0, gamma, policy):
+    '''
+    
+    Compute the Q function of a policy.
 
+    '''
+    sum    = 0
+    state  = state_0
+    action = action_0
+    for k in range(MAX_ITER):
+        sum    = sum + gamma**k * reward(state, action)
+        state  = evolve_state(state, policy)
+        action = policy(state)
+    return sum
+    
+def PE(gamma,policy,reward,value_function,epsilon):
+    '''
+
+    Policy evaluation.
+
+    '''
+    V_plus = np.zeros(20)
+    V = np.array([value_function(i+1, gamma, policy) for i in range(20)])
+    while np.linalg.norm(V_plus - V) > epsilon:
+        V = V_plus
+        for i in range(20):
+            V_plus[i] = reward(i+1,policy(i+1)) + gamma * value_function(evolve_state(i+1,policy), gamma, policy)
+    V = V_plus
+    return V
+
+def PE_hyst(gamma,policy,reward,value_function,epsilon, max_iter):
+    '''
+
+    Policy evaluation.
+
+    '''
+    V_hyst = []
+    V = np.zeros(20)
+    V_plus = np.zeros(20)
+    for _ in range(max_iter):
+        V[:] = V_plus[:]
+        for i in range(20):
+            V_plus[i] = reward(i+1,policy(i+1)) + gamma * V[evolve_state(i+1,policy)-1] #value_function(evolve_state(i+1,policy), gamma, policy)
+        V_hyst.append(V_plus)
+
+        if np.linalg.norm(V_plus - V) < epsilon:
+            break
+        
+    V = V_plus
+    
+    return V, V_hyst
+
+
+def exercise1(gammas):
+    '''
+    
+    Exercise 1.
+
+    '''
+    print("-"*NUM_DASHES)
+    print("Exercise 1")
     for gamma in gammas:
-        print("-"*20)
         print(f"gamma = {gamma}")
-        print("-"*20)
-        print(f"V_pi1 = {V_pi(1, gamma, pi1)}")
-        print(f"V_pi2 = {V_pi(1, gamma, pi2)}")
-        print("-"*20)
 
         reward_P1 = [reward(i, pi1(i)) for i in range(1, 21)]
         reward_P2 = [reward(i, pi2(i)) for i in range(1, 21)]
@@ -134,9 +189,90 @@ def main():
 
         V_bell_P1 = np.linalg.inv( np.eye(20)-gamma*P1 ) @ reward_P1
         V_bell_P2 = np.linalg.inv( np.eye(20)-gamma*P2 ) @ reward_P2
-        print(f"V_pi1 (Bellman) = {V_bell_P1[0]}")
-        print(f"V_pi2 (Bellman) = {V_bell_P2[0]}")
-        print("-"*20)
 
+        # print a formatted table with value of V_pi1, V_pi2, V_pi1 (Bellman), V_pi2 (Bellman)
+        print(f"{'State':^6} {'V_pi1':^15} {'V_pi1 (Bellman)':^15} {'V_pi2':^15} {'V_pi2 (Bellman)':^15}")
+        for i in range(20):
+            print(f"{i+1:>6} {V_pi(i+1, gamma, pi1):>15.4e} {V_bell_P1[i]:>15.4e} {V_pi(i+1, gamma, pi2):>15.4e} {V_bell_P2[i]:>15.4e}")
+        
+        print("-"*NUM_DASHES)
+
+
+def exercise2(action, gammas):
+    '''
+    
+    Exercise 2.
+
+    '''
+    print("-"*NUM_DASHES)
+    print("Exercise 2")
+    for gamma in gammas:
+        print(f"gamma = {gamma}")
+
+        reward_P1 = [reward(i, pi1(i)) for i in range(1, 21)]
+        reward_P2 = [reward(i, pi2(i)) for i in range(1, 21)]
+
+        P1 = state_transition_matrix_P1()
+        P2 = state_transition_matrix_P2()
+
+        V_bell_P1 = np.linalg.inv( np.eye(20)-gamma*P1 ) @ reward_P1
+        V_bell_P2 = np.linalg.inv( np.eye(20)-gamma*P2 ) @ reward_P2
+
+        # print a formatted table with value of V_pi1, V_pi2, V_pi1 (Bellman), V_pi2 (Bellman)
+        print(f"{'State':^6} {'Q_pi1':^15} {'Q_pi2':^15} {'Err. Q_pi1':^15} {'Err. Q_pi2':^15} {'Err. Q_pi1 (Bellman)':^15} {'Err. Q_pi2 (Bellman)':^15}")
+        for i in range(20):
+            Q_pi1_tmp = Q_pi(i+1, action, gamma, pi1)
+            Q_pi2_tmp = Q_pi(i+1, action, gamma, pi2)
+            diff_Q_pi1 = Q_pi(i+1, pi1(i+1), gamma, pi1) - V_pi(i+1, gamma, pi1)
+            diff_Q_pi2 = Q_pi(i+1, pi2(i+1), gamma, pi2) - V_pi(i+1, gamma, pi2)
+            diff_Q_pi1_bell = Q_pi(i+1, pi1(i+1), gamma, pi1) - V_bell_P1[i]
+            diff_Q_pi2_bell = Q_pi(i+1, pi2(i+1), gamma, pi2) - V_bell_P2[i]
+
+            print(f"{i+1:>6} {Q_pi1_tmp:>15.4e} {Q_pi2_tmp:>15.4e} {diff_Q_pi1:>15.4e} {diff_Q_pi2:>15.4e} {diff_Q_pi1_bell:>15.4e} {diff_Q_pi2_bell:>15.4e}")
+
+        
+        print("-"*NUM_DASHES)
+
+def exercise3(gammas):
+    '''
+    
+    Exercise 3.
+
+    '''
+    print("-"*NUM_DASHES)
+    print("Exercise 3")
+    for gamma in gammas:
+        print(f"gamma = {gamma}")
+        reward_P1 = [reward(i, pi1(i)) for i in range(1, 21)]
+        reward_P2 = [reward(i, pi2(i)) for i in range(1, 21)]
+
+        P1 = state_transition_matrix_P1()
+        P2 = state_transition_matrix_P2()
+
+        V_bell_P1 = np.linalg.inv( np.eye(20)-gamma*P1 ) @ reward_P1
+        V_bell_P2 = np.linalg.inv( np.eye(20)-gamma*P2 ) @ reward_P2
+
+        V, V_hyst = PE_hyst(gamma, pi1, reward, V_pi, 1e-8, 20)
+
+        err_hyst = [np.linalg.norm(V_tmp - V_bell_P1) for V_tmp in V_hyst]
+
+        plt.figure()
+        plt.plot(err_hyst)
+        plt.show()
+            
+
+
+
+
+
+
+
+def main():
+    gammas = (0.5, 0.85, 0.9, 0.99, 1-1e-5) #, 1 -> omitted for singularity
+    # exercise1(gammas)
+    # exercise2(2,gammas)
+    exercise3(gammas)
+
+    
 if __name__ == "__main__":
     main()
---
+++ Assignment1.md
@@ -0,0 +1,24 @@
+# Assignment 1
+
+## 1.a
+
+[add table with results]
+
+
+
+What do you observe when the discount factor changes?
+
+
+The value function takes longer to converge when the discount factor is closer to one. Additionally, when $\gamma = 1$ and we use policy $\pi_1$ the $(I-\gamma P_{ij}^{\pi(s)})$ is singular.
+
+## 1.b
+
+[add table with results]
+
+Are these policies optimal? 
+
+Not necessarily, we are not guaranteed to find the optimal policy. ???
+
+What do you observe when the discount factor changes?
+
+The same consideration as in 1.a applies here.
